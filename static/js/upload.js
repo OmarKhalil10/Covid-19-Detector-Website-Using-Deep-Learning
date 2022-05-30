14246364672a28664e78a77fcd9eadd30526d386
John Aziz <johnaziz269@gmail.com>
2022-05-30 03:50:57 +0200

    Submit and Cancel Buttons

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,5 +1,5 @@
 //selecting all required elements
-const dropArea = document.querySelector(".drag-area"),
+var dropArea = document.querySelector(".drag-area"),
 dragText = dropArea.querySelector(".drag-drop"),
 button = dropArea.querySelector(".upload-btn"),
 input = dropArea.querySelector(".upload-file");
@@ -38,7 +38,7 @@ function showFile(){
     let fileReader = new FileReader(); //creating new FileReader object
     fileReader.onload = ()=>{
       let fileURL = fileReader.result; //passing user file source in fileURL variable
-      let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
+      let imgTag = `<img id="uploaded-image" src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
       dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
     }
     fileReader.readAsDataURL(file);
