8b428d461872a515b87cd8bd3d22461fba0f5dcd
OmarKhalil10 <omar.khalil498@gmail.com>
2023-03-14 18:15:17 +0200

    update environment variables

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
-import os 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), 'vars.env')
+load_dotenv(dotenv_path)
 
 UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
 SECRET_KEY=os.environ.get('SECRET_KEY')
